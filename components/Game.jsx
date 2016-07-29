f71cdb106780b1ec7eef89f3c2a87fe15ce92183
reedwilliams24 <reedwilliams24@gmail.com>
2016-07-28 18:26:11 -0600

    add timer

--- components/Game.jsx
+++ components/Game.jsx
@@ -5,12 +5,26 @@ var Minesweeper = require('../minesweeper');
 var Game = React.createClass({
   getInitialState: function() {
     var board = new Minesweeper.Board(10, 10);
-    return { board: board };
+    return {
+      board: board,
+      secondsElapsed: 0
+    };
+  },
+
+  componentDidMount: function() {
+    window.setInterval(function(){
+      this.setState({
+        secondsElapsed: this.state.secondsElapsed + 1
+      });
+    }.bind(this), 1000);
   },
 
   updateGame: function(tile, flagged) {
     if (flagged){
       tile.toggleFlag();
+      if (this.state.board.numBombs - this.state.board.flagCount < 0) {
+        tile.toggleFlag();
+      }
     } else {
       tile.explore();
     }
@@ -30,18 +44,35 @@ var Game = React.createClass({
     }
 
     return (
-      <div>
-        <div id='scoreboard'>
-          <div id='flagCount'>
+      <div id='game'>
+        <div id='info'>
+          <div id='flag-count'>
             {this.state.board.numBombs - this.state.board.flagCount}
           </div>
-          <div>
-            {gameText}
+          <div id='timer'>
+            {this.printTime()}
           </div>
         </div>
         <Board board={this.state.board} updateGame={this.updateGame}/>
+        <div>
+          {gameText}
+        </div>
       </div>
     );
+  },
+
+  printTime: function(){
+    var seconds = this.state.secondsElapsed;
+    var minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+
+    var secondsString = seconds.toString();
+    if (seconds < 10) secondsString = '0' + secondsString;
+
+    var minutesString = minutes.toString();
+    if (minutes < 10) minutesString = '0' + minutesString;
+
+    return minutesString + ':' + secondsString;
   }
 
 });

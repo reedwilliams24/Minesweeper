d477ab1bd59a561f52137ed972fa3b300ffa6a42
reedwilliams24 <reedwilliams24@gmail.com>
2016-07-30 20:23:28 -0600

    add images

--- components/Tile.jsx
+++ components/Tile.jsx
@@ -42,11 +42,11 @@ var Tile = React.createClass({
     var tile = this.props.tile;
 
     if (tile.flagged){
-      return 'F';
+      return <img src='./docs/flag.png'></img>;
     } else if (!tile.explored){
       return '';
     } else if (tile.mine){
-      return 'X';
+      return <img src='./docs/mine.png'></img>;
     } else {
       var mineCount = this.props.board.adjacentMineCount(tile);
       if (mineCount !== 0) return mineCount;
